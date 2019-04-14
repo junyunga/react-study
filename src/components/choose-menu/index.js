bab3d836089b32acb9041870259a5e6c8369f6b5
룡이 <vev4747@gmail.com>
2019-04-14 18:38:01 +0900

    chosseMenu 스타일 수정

--- src/components/choose-menu/index.js
+++ src/components/choose-menu/index.js
@@ -1,19 +1,28 @@
 import React, {Component} from 'react';
 import {Link} from "react-router-dom";
+import './chooseMenu.scss';
 
 class ChooseMenu extends Component {
     render() {
         return(
             <div className="choose-menu">
-                <h1>업무선택</h1>
+                <h1 className="title">업무선택</h1>
                 <div className="card-container">
-                    <div className="deportment-row">
-                        <div className="deportment-card"><Link to="/product">생산</Link></div>
-                        <div className="deportment-card"><Link to="/sale">영업</Link></div>
+                    <div className="department-row">
+                        <div className="department-card">
+                            <Link className="link-wrapper" to="/product">생산</Link>
+                        </div>
+                        <div className="department-card">
+                            <Link className="link-wrapper" to="/sale">영업</Link>
+                        </div>
                     </div>
-                    <div className="deportment-row">
-                        <div className="deportment-card"><Link to="/quality">품질</Link></div>
-                        <div className="deportment-card"><Link to="/purchase">외주/구매</Link></div>
+                    <div className="department-row">
+                        <div className="department-card">
+                            <Link className="link-wrapper" to="/quality">품질</Link>
+                        </div>
+                        <div className="department-card">
+                            <Link className="link-wrapper" to="/purchase">외주/구매</Link>
+                        </div>
                     </div>
                 </div>
             </div>
