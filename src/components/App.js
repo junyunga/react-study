2a1a931b2dd7114bb1b113c6fa85c65f716a7cec
룡이 <vev4747@gmail.com>
2019-04-14 18:10:39 +0900

    scss 적용
    빠진것 다시 추가

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Route, Switch} from 'react-router-dom';
 import ChooseMenu from 'components/choose-menu';
 import Layout from 'components/layout';
+import './App.scss';
 
 class App extends Component {
     render() {
