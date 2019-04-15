0759e15537420b9be925783eecfe40af08f47f39
룡이 <vev4747@gmail.com>
2019-04-16 01:35:41 +0900

    right-side 에도 route 추가

---
+++ src/components/material-insert/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+class MaterialInsert extends Component {
+    render() {
+        return(
+            <div className="material-insert">
+                자재투입
+            </div>
+        )
+    }
+}
+
+export default MaterialInsert;
--- src/components/right-side/index.js
+++ src/components/right-side/index.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
+import {Route, Switch} from 'react-router-dom';
+import MaterialInsert from 'components/material-insert';
 
 class RightSide extends Component  {
     render() {
         return (
             <div className="right-side">
-                right-side
+                <Switch>
+                    <Route path="/product/material-insert" component={MaterialInsert}/>
+                </Switch>
             </div>
         )
     }
