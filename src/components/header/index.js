c3a889ad5bfec584bf78d1f88144c444e558e5e9
룡이 <vev4747@gmail.com>
2019-04-21 15:36:44 +0900

    헤더 수정 및 로고 추가
    멋지지 않나?ㅋㅋㅋ

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,7 +5,12 @@ class Header extends Component  {
     render() {
         return (
             <div className="header layout-container">
-                header
+                <div className="logo-wrapper">
+                    리 액 트 스 터 디
+                </div>
+                <div className="machine-list-wrapper">
+                    설비 리스트 들어올 자리
+                </div>
             </div>
         )
     }
