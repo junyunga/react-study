0e3f66cfad8d0dbd83ea47d296ff393811703901
룡이 <vev4747@gmail.com>
2019-04-21 16:38:35 +0900

    리액트 로고 2차 수정
    1.애니메이션 추가
    2. 색변경

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,15 +1,18 @@
 import React, {Component} from 'react';
 import './index.scss';
 
-class Header extends Component  {
+class Header extends Component {
     render() {
         return (
             <div className="header layout-container">
                 <div className="logo-wrapper">
-                    리 액 트 스 터 디
+                    <img className="logo"
+                         alt="리액트로고"
+                         src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png"/>
+                    <div className="logo-title">리액트스터디</div>
                 </div>
                 <div className="machine-list-wrapper">
-                    설비 리스트 들어올 자리
+
                 </div>
             </div>
         )
