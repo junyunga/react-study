9e9898629fa639ae9d1640e7cfb331fc1a53c50a
JunYoung Cho <cjy874545@gmail.com>
2019-05-09 02:19:47 +0900

    작업현황 각 컴포넌트 타이틀 작업

--- src/components/work-list/not-work/index.js
+++ src/components/work-list/not-work/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './index.scss';
 
 export default class NotWork extends Component {
@@ -7,11 +7,18 @@ export default class NotWork extends Component {
             <div className="not-work">
                 <div className="not-work-container">
                     <div className="not-work-content">
-                        not-work
+                        <div className="not-work-content__header">
+                            <div className="header__title">
+                                비가동 등록 여부
+                            </div>
+                            <div className="header__legend">
+                            </div>
+                        </div>
+                        <div className="not-work-content__body">
+                        </div>
                     </div>
                 </div>
             </div>
-
         );
     }
 }
--- src/components/work-list/production/index.js
+++ src/components/work-list/production/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './index.scss';
 
 export default class Production extends Component {
@@ -7,7 +7,15 @@ export default class Production extends Component {
             <div className="production">
                 <div className="production-container">
                     <div className="production-content">
-                        production
+                        <div className="production-content__header">
+                            <div className="header__title">
+                                생산 실적 현황
+                            </div>
+                            <div className="header__legend">
+                            </div>
+                        </div>
+                        <div className="production-content__body">
+                        </div>
                     </div>
                 </div>
             </div>
